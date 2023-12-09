63b9065ffce8f388745d05e3ebef17ccb3f5f040
carolina gimenez <carolina.matrice@gmail.com>
2023-12-09 19:38:51 -0300

    estilos del login

--- src/features/home/views/home_view.jsx
+++ src/features/home/views/home_view.jsx
@@ -1,9 +1,10 @@
 import React from 'react'
 import { useAuth } from '../../../core/auth/hook/use_auth';
-import { AppSwiper }       from '../../../core/components/app_swiper/app_swiper';
-import  AppSwiperSlide  from '../../../core/components/app_swiper/components/app_swiper_slide';
+
 import { getPopularMovies } from '../services/movies.service';
+import { getTopRatedMovies } from '../services/movies.service';
 import useSWR from 'swr';
+import AppCarouselSection from '../../../core/components/app_carousel_section/app_carousel_section';
 
 const HomeView = () => {
 
@@ -12,31 +13,25 @@ const HomeView = () => {
   const {logout, isLoggedIn} = useAuth();
   console.log(isLoggedIn);
   
-  const { data, error, isLoading } = useSWR('getPopularMovies', getPopularMovies);
+  const { 
+    data: popularMovies, 
+    error: popularMoviesError, 
+    isLoading: popularMoviesIsLoading 
+  } = useSWR('getPopularMovies', getPopularMovies);
 
-  console.log(data);
+  const { 
+    data: topRatedMovies,  
+    error: topRatedMoviesError, 
+    isLoading: topRatedMoviesIsLoading 
+  } = useSWR('getTopRatedMovies', getTopRatedMovies);
+
+  console.log(popularMovies);
   return (
     <div>
       <button onClick={logout}>Cerrar Sesión</button>
       <br />
       <br />
-      <h1>Películas Mejor Puntuadas</h1>
-      <AppSwiper>
-      { data?.map((e, index) => (
-        <AppSwiperSlide key={index}>
-          <div style={{
-              height: '150px',
-              width: '250px',
-              backgroundImage: `url(${e.backdrop})`, 
-              backgroundSize: 'contain',
-              backgroundRepeat: 'no-repeat',
-              backgroundPosition: 'center',
-            }}>
-            <h3>{e.title}</h3>
-          </div>
-        </AppSwiperSlide>
-      ))}
-      </AppSwiper>
+      <AppCarouselSection title={"Peliculas Más Populares"} data={popularMovies} />
     </div>
   )
 }
--- src/features/login/views/login_view.jsx
+++ src/features/login/views/login_view.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import { useAuth } from '../../../core/auth/hook/use_auth'
+import 'bootstrap/dist/css/bootstrap.css'
 
 const LoginView = () => {
 
@@ -20,18 +21,33 @@ const LoginView = () => {
   };
 
   return (
-    <div>
-        <h1>¡Bienvenido!</h1>
+    <div className='container' style={{
+      backgroundImage: `url("/public/images/website_large.jpg")`,
+      //height:'680px',
+      //width: '100%',  
+      }}>
+      <div className='row'>
+          <div className='navbar navbar-dark bg-dark'></div>
+          <div className='col'></div>
+          <div className='col login-form'  style={{
+            backgroundColor: 'rgba(0,0,0,.75)',
+            borderRadius: '4px',
+            boxSizing: 'border-box',
+            padding: '60px 68px 40px',
+          }}>
 
-
-        <form onSubmit={handleSubmit}>
-          <input type="email" name="email" />
-          <br />
-          <input type="password" name="password" />
-          <br />
-          <button type="submit">Iniciar Sesión</button>
-        </form>
-
+          <div className='login-page-container'>
+            <h1 className='login-h1'>Inicia Sesión</h1>
+            <form  className="form-inc" onSubmit={handleSubmit}>
+              <input type="email" name="email" className='form-input border border-white' placeholder='Email o número de teléfono' />
+              <input type="password" name="password" className='form-input border border-white' placeholder='Contraseña'/>
+              <button type="submit" className='btn login-button'>Iniciar Sesión</button>
+              <a href="#" className='login-link'>¿Olvidaste la contraseña?</a>
+            </form>
+          </div>
+          </div>
+          <div className='col'></div>
+        </div>
       </div>
   )
 }
